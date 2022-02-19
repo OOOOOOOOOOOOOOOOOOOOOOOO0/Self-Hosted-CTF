010af587687827645246fb3dd4b26ffc638c0459
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-19 11:49:43 +0000

    Update server.py

--- Game/server.py
+++ Game/server.py
@@ -1,10 +1,111 @@
 from flask import Flask, request, redirect
 import json
 
-app = Fask(__name__)
-app.config['DEBUG'] = True
 
-with open('./details.json', 'r') as fp:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app = Flask(__name__)
+#app.config['DEBUG'] = True
+
+with open('Answer/details.json', 'r') as fp:
     config = json.load(fp)
 
 username = config['username']
@@ -20,6 +121,6 @@ def challenge():
   if request.method == 'POST' and request.form['username'] == username and request.form['password'] == password:
     return f"Congratulations! You completed the challenge, the flag is: {flag}\n\n\n\n\nIf you'd like to read the source code of this challenge: https://replit.com/@Argue/POST-CTF"
   else:
-    return f"<title>CTF</title> <h1> Welcome to Fin's POST Request CTF Challenge, this site takes <b>POST</b> data only, the challenge is to find the flag, goodluck </h1><!--- username: {username} | password: {password}, what can you do with this data?---!> <small> hint: fnhpr, i love rot13! </small>"
+    return f"<title>CTF</title> <h1> Welcome to C0mpt0's POST Request CTF Challenge, this site takes <b>POST</b> data only, the challenge is to find the flag, goodluck </h1><!--- username: {username} | password: {password}, what can you do with this data?---!> <small> hint: fnhpr, i love rot13! </small>"
 
-app.run(host="0.0.0.0", port=8080)
+app.run(host="127.0.0.1", port=1337)
